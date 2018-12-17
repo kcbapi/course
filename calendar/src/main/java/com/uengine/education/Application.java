2a23ec325abf0c2b1358b4b7649d86aae9ed8d76
kcbapi <nizzlea@gmail.com>
2018-12-17 16:46:14 +0900

    .

--- calendar/src/main/java/com/uengine/education/Application.java
+++ calendar/src/main/java/com/uengine/education/Application.java
@@ -2,8 +2,11 @@ package com.uengine.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+
 public class Application {
 
 	public static void main(String[] args) {
--- course/src/main/java/com/uengine/education/Application.java
+++ course/src/main/java/com/uengine/education/Application.java
@@ -3,7 +3,9 @@ package com.uengine.education;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableEurekaClient
 @Configuration
 @ComponentScan(basePackageClasses = Application.class)
 @EnableJpaRepositories(basePackageClasses = {Application.class})
